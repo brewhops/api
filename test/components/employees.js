47eed5063d7b178d8883cbbd00734984ee2ce812
Connor Christensen <chriconn@oregonstate.edu>
2018-06-25 15:05:17 -0700

    Updated the language on the employees test

--- test/components/employees.js
+++ test/components/employees.js
@@ -5,7 +5,7 @@ const agent = request.agent(app)
 
 describe('/employees', function () {
   describe('GET /employees', function () {
-    it('should respond with json', function (done) {
+    it('responds with json and a 200 code', function (done) {
       agent.get('/employees')
         .set('Accept', 'application/json')
         .expect('Content-Type', /json/)
@@ -14,21 +14,21 @@ describe('/employees', function () {
   })
 
   describe('GET /employees/0', function () {
-    it('should say that route does not exist', function (done) {
+    it('says that route does not exist', function (done) {
       agent.get('/employees/0')
         .expect(400, done)
     })
   })
 
   describe('GET /employees/id/0', function () {
-    it('should say that access is denied without a valid token', function (done) {
+    it('says that access is denied without a valid token', function (done) {
       agent.get('/employees/id/0')
         .expect(401, done)
     })
   })
 
   describe('POST /employees', function () {
-    it('should create a new user', function (done) {
+    it('creates a new user', function (done) {
       agent
         .post('/employees')
         .send({
@@ -39,14 +39,7 @@ describe('/employees', function () {
           'phone': 'phone',
           'access_level': 1
         })
-        .expect(201)
-        .end(function (err, res) {
-          if (err) {
-            console.log(res)
-            return done(err)
-          }
-          done()
-        })
+        .expect(201, done)
     })
   })
 })
