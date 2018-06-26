b9acf7be208efdca0c5c379af28610aea86b4797
Connor Christensen <chriconn@oregonstate.edu>
2018-06-25 18:16:58 -0700

    Created a basic test to check the connection before testing routes
    The basic test tests the port and the host. This is a safeguard for 
    people running a test and forgetting to change the environment 
    variables.

---
+++ test/dbSetup.js
@@ -0,0 +1,17 @@
+/* global describe it */
+
+// import config info
+require('dotenv').config()
+
+const chai = require('chai')
+const expect = chai.expect
+
+describe('Database connect', function () {
+  it('is using port 32769', function () {
+    expect(process.env.PGPORT).to.equal('32769')
+  })
+
+  it('is using host localhost', function () {
+    expect(process.env.PGHOST).to.equal('localhost')
+  })
+})
