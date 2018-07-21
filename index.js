926a9f4757230ed90733bffa4a85994a83287083
Connor Christensen <chriconn@oregonstate.edu>
2018-07-20 23:02:49 -0700

    Installed CORS package
    This makes the whole CORS system easier and makes it so that the website 
    can set the right headers so the local client instance can connect to 
    the local server version.

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 require('dotenv').config()
 let app = require('express')()
 let bodyParser = require('body-parser')
+const cors = require('cors')
 
 if (process.env.NODE_ENV !== 'test') {
   app.listen(process.env.PORT, () => console.log(`Server running at port ${process.env.PORT}`))
 }
 
 app.use(bodyParser.json())
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-  next()
-})
+app.use(cors())
 
 app.use(function(err, req, res, next) {
   console.log(err)
