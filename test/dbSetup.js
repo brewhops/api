a7cda2a1b1e5fa18caf839974314659c936c780b
Connor Christensen <chriconn@oregonstate.edu>
2018-07-26 01:26:44 -0700

    Few more comments

--- test/dbSetup.js
+++ test/dbSetup.js
@@ -6,6 +6,7 @@ require('dotenv').config()
 const chai = require('chai')
 const expect = chai.expect
 
+// these tests are to make sure the user set up the .env file correctly.
 describe('Database connect', function() {
   it('is using port 45758', function() {
     expect(process.env.TEST_PG_PORT).to.equal('45758')
--- test/tableNames.js
+++ test/tableNames.js
@@ -1,1 +1,2 @@
+// just an array of all the table names in the db
 module.exports = [ 'employees', 'actions', 'tanks', 'recipes', 'batches', 'versions', 'tasks' ]
