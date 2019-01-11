686ec88076db5ceda043636e05230e3b2af2bf55
Brennan Douglas <brennand97@gmail.com>
2019-01-11 09:58:23 -0800

    added admin user insert script

---
+++ db-init/initial_data.js
@@ -0,0 +1,35 @@
+require('dotenv').config()
+const CryptoJS = require("crypto-js");
+const employees = require("../src/components/employees/logic");
+
+function encryptPassword(password, username) {
+	return CryptoJS.AES.encrypt(password, username).toString();
+}
+
+async function insertData() {
+	
+	let Employees = new employees('employees');
+	Employees.connectToDB()
+		.then(() => console.log('Employees route connected to database'))
+		.catch(e => console.log('Error! Connection refused', e));
+
+	let user = {
+		first_name: "General",
+		last_name: "Kenobi",
+		username: "admin",
+		phone: "5555555555",
+		admin: true
+	};
+	user.password = encryptPassword("password", user.username);
+	
+	try {
+		const { keys, values, escapes } = Employees.splitObjectKeyVals(user);
+		await Employees.create(keys, escapes, values);
+		console.log('Success!');
+	} catch(e) {
+		console.log('Error!', e)
+	}
+	
+}
+
+insertData().then(() => process.exit());
