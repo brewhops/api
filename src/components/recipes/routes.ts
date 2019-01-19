a7fc1a45f7e33c09a0f32c671d9df2098482cb0b
Dan Van Horn <dan.vanhorn17@gmail.com>
2019-01-18 17:36:01 -0800

    feat(recipes): add validation to create, update, delete operations

--- src/components/recipes/routes.ts
+++ src/components/recipes/routes.ts
@@ -2,6 +2,7 @@ import e, { Request, Response, NextFunction, Router } from 'express';
 import { RecipeController, IRecipeController } from './controller';
 import { RecipeValidator } from './validator';
 import Boom from 'boom';
+import { requireAuthentication } from '../../middleware/auth';
 // tslint:disable-next-line:no-var-requires no-require-imports
 const validate = require('express-validation');
 
@@ -19,13 +20,13 @@ export function routes(): Router {
   router.get('/id/:id', async (req, res, next) => controller.getRecipe(req, res, next));
 
   // [POST] section
-  router.post('/', validate(RecipeValidator.createRecipe), async (req, res, next) => controller.createRecipe(req, res, next));
+  router.post('/', validate(RecipeValidator.createRecipe), requireAuthentication, async (req, res, next) => controller.createRecipe(req, res, next));
 
   // [PATCH] section
-  router.patch('/id/:id', validate(RecipeValidator.updateRecipe), async (req, res, next) => controller.updateRecipe(req, res, next));
+  router.patch('/id/:id', validate(RecipeValidator.updateRecipe), requireAuthentication, async (req, res, next) => controller.updateRecipe(req, res, next));
 
   // [DELETE] section
-  router.delete('/id/:id', async (req, res, next) => controller.deleteRecipe(req, res, next));
+  router.delete('/id/:id', requireAuthentication, async (req, res, next) => controller.deleteRecipe(req, res, next));
 
   router.use('*', (req, res) => res.json(Boom.badRequest(`${req.originalUrl} doesn't exist`)));
 
