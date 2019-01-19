9be37da827ec863c5d1f3a02d67325d5e805dd0f
Dan Van Horn <dan.vanhorn17@gmail.com>
2019-01-18 16:48:27 -0800

    refactor(batches): update batches with comments and type signatures

--- src/components/batches/routes.ts
+++ src/components/batches/routes.ts
@@ -1,5 +1,5 @@
 import e, { Router, Request, Response, NextFunction } from 'express';
-import { BatchesLogic } from './controller';
+import { BatchesController, IBatchesController } from './controller';
 import { BatchesValidator } from './validator';
 import Boom from 'boom';
 // tslint:disable-next-line:no-var-requires no-require-imports
@@ -7,29 +7,29 @@ const validate = require('express-validation');
 
 // tslint:disable:no-any no-unsafe-any
 
-export function routes() {
-  const controller = new BatchesLogic('batches');
-  const router = Router();
+export function routes(): Router {
+  const controller: IBatchesController = new BatchesController('batches');
+  const router: Router = Router();
 
   // tslint:disable-next-line:no-void-expression
   router.use((req: Request, res: Response, next: NextFunction) => next());
 
   // [GET] section
-  router.get('/', controller.getBatches);
-  router.get('/id/:id', controller.getBatch);
-  router.get('/history/id/:id', controller.getBatchHistory);
+  router.get('/', async (req, res, next) => controller.getBatches(req, res, next));
+  router.get('/id/:id', async (req, res, next) => controller.getBatch(req, res, next));
+  router.get('/history/id/:id', async (req, res, next) => controller.getBatchHistory(req, res, next));
 
   // [POST] section
-  router.post('/', validate(BatchesValidator.createBatch), controller.createBatch);
+  router.post('/', validate(BatchesValidator.createBatch), async (req, res, next) => controller.createBatch(req, res, next));
 
   // [PATCH] section
-  router.patch('/id/:id', validate(BatchesValidator.updateBatch), controller.updateBatch);
+  router.patch('/id/:id', validate(BatchesValidator.updateBatch), async (req, res, next) => controller.updateBatch(req, res, next));
 
   // [DELETE] section
-  router.delete('/id/:id', controller.deleteBatch);
+  router.delete('/id/:id', async (req, res, next) => controller.deleteBatch(req, res, next));
 
   router.use('*', (req, res) =>
-    res.status(404).json(Boom.notFound(`The route ${req.originalUrl} does not exist`))
+    res.send(Boom.notFound(`The route ${req.originalUrl} does not exist`))
   );
 
   return router;
