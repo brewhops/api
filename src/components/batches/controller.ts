dd4e47dd11fdb57d6d2a305547db8b4c996f3a78
Henry Peterson <peterhen@oregonstate.edu>
2019-01-30 23:09:05 -0800

    feat(tank-history): add endpoint to get batches by tank

--- src/components/batches/controller.ts
+++ src/components/batches/controller.ts
@@ -4,6 +4,7 @@ import Boom from 'boom';
 
 export interface IBatchesController extends IPostgresController {
   getBatches: RequestHandler;
+  getBatchesByTank: RequestHandler;
   getBatch: RequestHandler;
   createBatch: RequestHandler;
   updateBatch: RequestHandler;
@@ -33,6 +34,24 @@ export class BatchesController extends PostgresController implements IBatchesCon
     try {
       await this.connect();
       const { rows } = await this.read('*', '$1', [true]);
+      res.status(200).json(rows);
+    } catch (err) {
+      res.status(500).send(Boom.badImplementation(err));
+    }
+    await this.disconnect();
+  }
+
+  /**
+   * Returns all versions from the cooresponding batch
+   * @param {Request} req
+   * @param {Response} res
+   * @param {NextFunction} next
+   * @memberof TaskController
+   */
+  async getBatchesByTank(req: Request, res: Response, next: NextFunction) {
+    try {
+      await this.connect();
+      const { rows } = await this.read('*', 'tank_id = $1', [req.params.id]);
       res.status(200).json(rows);
     } catch (err) {
       res.status(500).send(Boom.badImplementation(err));
