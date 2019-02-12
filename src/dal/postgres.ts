04f2b27f91c5249e022f3c7854e3d66177bae297
Brennan Douglas <brennand97@gmail.com>
2019-02-11 20:50:52 -0800

    chore(batches): responded to comments, cleaned up logic

--- src/dal/postgres.ts
+++ src/dal/postgres.ts
@@ -40,9 +40,10 @@ export class PostgresController extends CrudController implements IPostgresContr
     const values = [];
     const escapes = [];
     let idx = 1;
+
     // tslint:disable-next-line: no-for-in forin
     for (const key in obj) {
-      if (obj[key] !== undefined && obj[key] !== null) {
+      if (obj[key]) {
         keys.push(key.toString());
         values.push(obj[key].toString());
         escapes.push(`\$${idx}`); // eslint-disable-line
