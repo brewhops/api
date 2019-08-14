2fffddde8ac1710b81324deb6d68d3b74bb6787b
danvanhorn <dan.vanhorn17@gmail.com>
2019-08-14 06:11:44 -0700

    feat: use cors and body parser for batches route

--- src/components/batches/index.ts
+++ src/components/batches/index.ts
@@ -1,7 +1,14 @@
+import bodyParser from "body-parser";
 import e from "express";
 import { routes } from "./routes";
 
+// tslint:disable-next-line: no-var-requires
+const cors = require("cors");
+
 const lambda = e();
+
+lambda.use(bodyParser.json());
+lambda.use(cors());
 
 lambda.use("/batches", routes());
 
