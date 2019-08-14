7c45d7b346df898f01de17aae3423dd8b586af27
danvanhorn <dan.vanhorn17@gmail.com>
2019-08-13 17:49:39 -0700

    feat: add connection info for staging db

--- src/dal/db.ts
+++ src/dal/db.ts
@@ -1,5 +1,4 @@
 import { Pool, PoolConfig } from "pg";
-import { decrypt } from "../utils/decrypt";
 import { ca, cert, key } from "./certs";
 
 let config: PoolConfig = {
@@ -23,24 +22,24 @@ if (process.env.NODE_ENV === "test") {
 if (process.env.IS_NOW) {
     const { NODE_ENV } = process.env;
     if (NODE_ENV === "production") {
-        const { DB_USER: user, DB_NAME: database, DB_HOST: host, DB_PASSWORD: password, DB_PORT: port } = process.env;
-        config = { ...config, user, database, password, host, port: port as number | undefined };
-    } else if (NODE_ENV === "staging") {
         const {
-            DB_DEV_USER: user,
-            DB_DEV_NAME: database,
-            DB_DEV_HOST: host,
-            DB_DEV_PASSWORD: password,
+            DB_USER: user,
+            DB_NAME: database,
+            DB_HOST: host,
+            DB_PASSWORD: password,
             DB_PORT: port,
         } = process.env;
         config = { ...config, user, database, password, host, port: port as number | undefined };
+    } else if (NODE_ENV === "staging") {
+        const {
+            DB_STAGING_USER: user,
+            DB_STAGING_NAME: database,
+            DB_STAGING_HOST: host,
+            DB_STAGING_PASSWORD: password,
+            DB_STAGING_PORT: port,
+        } = process.env;
+        config = { ...config, user, database, password, host, port: port as number | undefined };
     }
-    config.ssl = {
-        ca: decrypt(ca),
-        cert: decrypt(cert),
-        key: decrypt(key),
-        rejectUnauthorized: false,
-    };
 }
 
 export const pool = new Pool(config);
